14804e9a826838a81fbbe81038c47179656155b8
BeautyOfCoding <BeautyOfCoding@mail.com>
2020-02-06 17:06:43 +0100

    Finally understoodGoodThe Loop FOR and WHILE(var)

---
+++ loops-excercises.js
@@ -0,0 +1,78 @@
+// for prompt ("hi")
+// let i = prompt("Haa!");
+
+// while (Number(i) <= 100)
+// {
+//     i =  prompt("Haa!");
+// }
+
+
+
+
+
+
+
+// let string = ""
+// for (let i=0; i<16;i++) {
+//     string += "Na"
+// }
+// console.log(`${string} ${"ButtMan"}`)
+
+
+
+// print all numbers from 1 to 107 included
+
+// let there = 'light'
+
+// let i = 0
+// console.log('after init', i)
+
+// while (i < 108) {
+//     // console.log('after checking', i)
+//     console.log(i)
+//     // i = i + 10
+//     i += 10
+//     // console.log('after re-assigning', i)
+// }
+
+// console.log('after loop', i)
+
+
+
+
+
+
+
+
+
+
+
+// init
+// while (condition) {
+//     body
+//     reassign
+// }
+
+// for (init; condition; reassign) {
+//     body
+// }
+
+
+// let i =0;
+
+// for( i = 0; i < 108; i += 10) {
+//     console.log(i)
+
+// }
+
+// console.log(i+'Result!')
+
+
+let i
+
+for (i = 0; i < 108; i += 10) {
+    console.log(i)
+
+}
+
+console.log(i+'Result!')
