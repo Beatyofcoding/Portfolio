91de4638c8412b1bdd48c9ba4b2979eb8f813a35
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-04-10 01:20:26 +0200

    Starting with Javascript part of the Calculator

---
+++ Calculator HTML CSS JAVASCRIPT/index.js
@@ -0,0 +1,26 @@
+var oneBtn = document.getElementById('calc-one');
+var twoBtn = document.getElementById('calc-two');
+var threeBtn = document.getElementById('calc-three');
+var fourBtn = document.getElementById('calc-four');
+var fiveBtn = document.getElementById('calc-five');
+var sixBtn = document.getElementById('calc-six');
+var sevenBtn = document.getElementById('calc-seven');
+var eightBtn = document.getElementById('calc-eight');
+var niceBtn = document.getElementById('calc-nine');
+var zeroBtn = document.getElementById('calc-zero');
+
+var decimalBtn = document.getElementById('calc-decimal');
+var clearBtn = document.getElementById('calc-clear');
+var backspaceBtn = document.getElementById('calc-backspace');
+var displayValElement = document.getElementById('calc-display-val');
+
+var calcNumBtns = document.getElementByClassName('calc-btn-num');
+var oneBtn = document.getElementByIdClassName('calc-btn-operator');
+
+for (let i = 0; i < calcNumBtns.length; i++){
+    calcNumBtns[i].addEventListener('click', )
+
+}
+for (let i = 0; i < calcOperatorBtns.length; i++){
+    
+}
