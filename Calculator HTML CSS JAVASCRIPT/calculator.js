a17b9629ee87afdef627b667cb6424d1d038bbad
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-04-10 03:22:37 +0200

    Still with javascript

--- Calculator HTML CSS JAVASCRIPT/calculator.js
+++ Calculator HTML CSS JAVASCRIPT/calculator.js
@@ -1,3 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var oneBtn = document.getElementById('calc-one');
 var twoBtn = document.getElementById('calc-two');
 var threeBtn = document.getElementById('calc-three');
@@ -14,13 +57,33 @@ var clearBtn = document.getElementById('calc-clear');
 var backspaceBtn = document.getElementById('calc-backspace');
 var displayValElement = document.getElementById('calc-display-val');
 
+var displayVal = '0';
+var pendingVal;
+var evalStringArray = [];
+
 var calcNumBtns = document.getElementByClassName('calc-btn-num');
-var oneBtn = document.getElementByIdClassName('calc-btn-operator');
+var calcOperatorBtns = document.getElementByClassName('calc-btn-operator');
 
-for (let i = 0; i < calcNumBtns.length; i++){
-    calcNumBtns[i].addEventListener('click', )
+var updateDisplayVal = (clickObj) => {
+    var btnText = clickObj.target.innerText;
 
+    if(displayVal === '0')
+        displayVal = '';
+
+        displayVal += btnText;
+        displayValElement.innetText = displayVal;
 }
-for (let i = 0; i < calcOperatorBtns.length; i++){
+
+for (let i = 0; i < calcNumBtns.length; i++) {
+    calcNumBtns[i].addEventListener('click', updateDisplayVal, false);
+}
+
+// for (let i = 0; i < calcOperatorBtns.length; i++){
+//     calcOperatorBtns[i].addEventListener('click', updateDisplayVal, false);
     
+// }
+clearBtn.onclick = () => {
+    displayVal = '0';
+    pendingVal = undefined;
+    evalStringArray = [];
 }
